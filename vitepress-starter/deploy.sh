8477b88d3a101cc857a93571a5684f3aa07688e8
宋辰轩 <songchenxuan@easycorp.ltd>
2024-04-24 11:02:43 +0800

    * Use token.

--- vitepress-starter/deploy.sh
+++ vitepress-starter/deploy.sh
@@ -13,8 +13,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# Jacqueline712/mondaylab-blog 替换为自己的用户名和对应的仓库名
-# 意思为将master构建后的代码合并到gh-pages分支上，然后在gh-pages分支上部署~
-git push -f git@github.com:wannaQac/vitepress-blog.git master:gh-pages
+# 意思为将master构建后的代码合并到gh-pages分支上，然后在gh-pages分支上部署~, 替换<token>为git的token
+git push -f https://<token>@github.com/wannaQac/vitepress-blog.git/ master:gh-pages
 
 cd -
