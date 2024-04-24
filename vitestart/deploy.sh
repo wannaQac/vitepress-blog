41496d9f348e743283544cc88dab3691d34c4e3d
宋辰轩 <songchenxuan@easycorp.ltd>
2024-04-24 11:22:50 +0800

    * 终于整明白了.

--- vitestart/deploy.sh
+++ vitestart/deploy.sh
@@ -16,4 +16,7 @@ git commit -m 'deploy'
 # 意思为将master构建后的代码合并到gh-pages分支上，然后在gh-pages分支上部署~, 替换<token>为git的token
 git push -f https://<token>@github.com/wannaQac/vitepress-blog.git/ master:gh-pages
 
+# 更改token后为了避免提交到github，可以使用 git update-index --assume-unchanged vitestart/deploy.sh 来取消git对文件的跟踪
+# 可以使用 git update-index --no-assume-unchanged vitestart/deploy.sh 来恢复git对文件的跟踪
+
 cd -
